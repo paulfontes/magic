1705a5ae2c4e96b4ca56a2b46e4613f7805498c9
paulfontes <pgfon22@gmail.com>
2026-01-18 12:57:33 -0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup></script>
+
+<template>
+  <h1>You did it!</h1>
+  <p>
+    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> Time to make a
+    web application!! web application!!
+  </p>
+</template>
+
+<style scoped></style>
